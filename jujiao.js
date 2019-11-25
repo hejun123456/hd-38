50eba04f82032cb85f642e347697dd1fa3a0fd9e
hejun <1084276061@qq.com>
2019-11-25 11:07:46 +0800

    Initial commit

---
+++ jujiao.js
@@ -0,0 +1,18 @@
+// function my_focus() {
+//     var _input=document.getElementsByName("username");
+//     _input[0].value="";
+// }
+// function  my_onblur() {
+//     var _output=document.getElementsByName("username");
+//     _output[0].value="hello"
+// }
+// function my_on() {
+//     var _img=document.getElementById(id="tupian")
+//     _img.width="400";
+//     _img.height="400";
+// }
+// function my_onset() {
+//     var _img2=document.getElementById(id="tupian")
+//     _img2.width="193";
+//     _img2.height="193";
+// }
---
+++ lianxi.py
@@ -0,0 +1,10 @@
+# a="abcd"
+# # print(a.split("b"))
+# a.join('ef')
+# print(a)
+
+
+# lis=[1,2,3]
+# lis.append(4)
+# lis=[1,2,3,4]
+# print(lis[0:3])
